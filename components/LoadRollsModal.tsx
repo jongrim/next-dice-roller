5b354f79c8fe736a18d66902a78743caa15d0fb0
Jon Grim <jonjongrim@gmail.com>
2020-05-25 01:04:20 -0400

    update save and load mechanism (#11)
    * update save and load mechanism
    
    * remove viewport

--- components/LoadRollsModal.tsx
+++ components/LoadRollsModal.tsx
@@ -1,63 +1,69 @@
 import * as React from 'react';
 import { Dialog } from '@reach/dialog';
 import { Box, Button, Flex, Heading, Text } from 'rebass';
-import { Checkbox, Label } from '@rebass/forms';
+import { Checkbox } from '@rebass/forms';
 import { configuredRoll } from './DiceSelectionForm/DiceSelectionForm';
 
 interface LoadRollsModalProps {
   isOpen: boolean;
+  loadedRolls: configuredRoll[];
   onDismiss: (e: React.SyntheticEvent, rolls?: configuredRoll[]) => void;
+  storedRollIds: configuredRoll['id'][];
 }
 
 const LoadRollsModal: React.FC<LoadRollsModalProps> = ({
   isOpen,
+  loadedRolls,
   onDismiss,
+  storedRollIds,
 }) => {
-  const [storedRolls, setStoredRolls] = React.useState<configuredRoll[]>([]);
-  const [checkedIndex, setCheckedIndex] = React.useState<{
-    [key: string]: boolean;
-  }>({});
-  React.useEffect(() => {
-    const localRolls = window.localStorage.getItem('rolls');
-    if (localRolls) {
-      setStoredRolls(JSON.parse(localRolls));
-    }
-  }, [setStoredRolls]);
+  const [rollsToLoad, setRollsToLoad] = React.useState<configuredRoll[]>([]);
   return (
     <Dialog isOpen={isOpen} onDismiss={onDismiss} aria-label="Load saved rolls">
       <Heading as="h3">Load saved rolls</Heading>
-      <Text fontSize={1}>Select which saved rolls to load</Text>
-      <Box as="form" mt={3}>
-        {storedRolls.map((roll, i) => (
-          <Flex
-            as="label"
-            htmlFor={`${roll.rollName}-${i}`}
-            alignItems="center"
-            key={`${roll.rollName}-${i}`}
-          >
-            <Checkbox
-              checked={checkedIndex[i]}
-              name={`${roll.rollName}-${i}`}
-              id={`${roll.rollName}-${i}`}
-              onChange={() =>
-                setCheckedIndex((checked) => {
-                  return checked[i]
-                    ? { ...checked, [i]: false }
-                    : { ...checked, [i]: true };
-                })
-              }
-            />
-            {roll.rollName}
-          </Flex>
-        ))}
+      <Text fontSize={1}>
+        Select which saved rolls to load into the current room
+      </Text>
+      <Box as="form" data-testid="load-rolls-form" mt={3}>
+        {storedRollIds.map((id) => {
+          const roll: configuredRoll = JSON.parse(
+            window.localStorage.getItem(id)
+          );
+          const alreadyLoaded = loadedRolls.some((r) => r.id === id);
+          return (
+            <Flex
+              as="label"
+              htmlFor={roll.id}
+              alignItems="center"
+              key={roll.id}
+            >
+              <Checkbox
+                checked={rollsToLoad.some((r) => r.id === id) || alreadyLoaded}
+                disabled={alreadyLoaded}
+                name={roll.id}
+                id={roll.id}
+                onChange={() =>
+                  setRollsToLoad((rolls) => {
+                    return rolls.some((r) => r.id === id)
+                      ? rolls.filter((r) => r.id !== id)
+                      : [...rolls, roll];
+                  })
+                }
+                sx={{
+                  'input:disabled ~ &': {
+                    color: 'secondary',
+                  },
+                }}
+              />
+              {roll.rollName}
+            </Flex>
+          );
+        })}
         <Button
           onClick={(e) => {
-            onDismiss(
-              e,
-              storedRolls.filter((r, i) => checkedIndex[i])
-            );
+            onDismiss(e, rollsToLoad);
           }}
-          mt={4}
+          mt={3}
         >
           Done
         </Button>

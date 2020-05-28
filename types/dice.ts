e4c6db4e196830663016b146492e2fe6abb59a3c
Jon Grim <jonjongrim@gmail.com>
2020-05-28 19:12:15 -0400

    Add d4 and d2 (#22)

--- types/dice.ts
+++ types/dice.ts
@@ -3,7 +3,19 @@ export interface DiceBlock {
   needs: number;
 }
 
+export type diceNeedsSubmission = {
+  d2?: number;
+  d4?: number;
+  d6?: number;
+  d8?: number;
+  d10?: number;
+  d12?: number;
+  d20?: number;
+  d100?: number;
+};
+
 export interface DiceInterface {
+  d2: DiceBlock;
   d4: DiceBlock;
   d6: DiceBlock;
   d8: DiceBlock;
